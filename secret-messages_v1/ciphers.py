2ce94a5ecbccbf269ca3a8198e03f65a578bea08
youssefjohn <youssefjohn89@gmail.com>
2018-09-18 19:52:34 +0100

    Change to atbash and keyword
    quick change

--- secret-messages_v1/ciphers.py
+++ secret-messages_v1/ciphers.py
@@ -18,13 +18,12 @@ class Atbash(Cipher):
 
     def encrypt(self, text):
         self.encrypted_text = []
-        self.index = None
         text = text.upper()
 
         for letter in text:
             if letter in self.alphabet:
-                self.index = self.alphabet.index(letter)
-                self.encrypted_text.append(self.cipher_alphabet[self.index])
+                index = self.alphabet.index(letter)
+                self.encrypted_text.append(self.cipher_alphabet[index])
 
         self.encrypted_text = ''.join(self.encrypted_text)
         print("Here is your Encrypted message:")
@@ -32,13 +31,12 @@ class Atbash(Cipher):
 
     def decrypt(self, text):
         self.decrypted_text = []
-        self.index = None
         text = text.upper()
 
         for letter in text:
             if letter in self.cipher_alphabet:
-                self.index = self.cipher_alphabet.index(letter)
-                self.decrypted_text.append(self.alphabet[self.index])
+                index = self.cipher_alphabet.index(letter)
+                self.decrypted_text.append(self.alphabet[index])
 
         self.decrypted_text = ''.join(self.decrypted_text)
         print("Here is your Decrypted message:")
@@ -46,13 +44,38 @@ class Atbash(Cipher):
 
 
 
-    # def decrypt(self):
-    #     for letter in text:
-    #         if letter in alphabet
+
+class Keyword(Cipher):
+    def __init__(self):
+        self.alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+        self.keyword_alphabet = ['K', 'R', 'Y', 'P', 'T', 'O', 'S', 'A', 'B', 'C', 'D', 'E', 'F',
+                                 'G', 'H', 'I' 'J', 'L', 'M', 'N', 'Q', 'U', 'V', 'W', 'X', 'Z']
+
+    def encrypt(self, text):
+        self.encrypted_text = []
+        text = text.upper()
+
+        for thing in text:
+            if thing in self.alphabet:
+                index = self.alphabet.index(thing)
+                self.encrypted_text.append(self.keyword_alphabet[index])
+
+        self.encrypted_text = ''.join(self.encrypted_text)
+        print("Here is your Enrypted message:")
+        print(self.encrypted_text)
 
 
+    def decrypt(self, text):
+        self.decrypted_text = []
+        text = text.upper()
 
+        for char in text:
+            if char in self.keyword_alphabet:
+                index = self.keyword_alphabet.index(char)
+                self.decrypted_text.append(self.alphabet[index])
 
-x = Atbash()
-x.encrypt("egreg")
-x.decrypt("saakdj")
+        self.decrypted_text = ''.join(self.decrypted_text)
+        print("Here is your Decrypted message:")
+        print(self.decrypted_text)
